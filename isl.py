35cd93659f9d651a71acc37cc2f27daf8ff83b1b
Sun Xiaoyu <yinuo42@outlook.com>
2025-01-13 22:41:21 +0800

    isl developing

--- isl.py
+++ isl.py
@@ -48,6 +48,7 @@ class ISL:
         self.callback_func = callback_func
         
         self._net = TSEncoder(input_dims=input_dims, output_dims=output_dims, hidden_dims=hidden_dims, depth=depth)
+        self.discriminator = ProjectionHead(input_dims=2*output_dims, hidden_dims=hidden_dims, output_dims=1)
       
         device = torch.device(device)
         if device == torch.device('cuda') and self.multi_gpu:
@@ -59,3 +60,72 @@ class ISL:
         # self.net = self._net
         self.net = torch.optim.swa_utils.AveragedModel(self._net)
         self.net.update_parameters(self._net)
+        
+        
+    def fit(self, X, y, shuffle_function='random', masks=None, epochs=None, verbose=True):
+        ''' Training the ISL model.
+        
+        Args:
+            X (numpy.ndarray): The training data. It should have a shape of (n_samples, sample_timestamps, features).
+            y (numpy.ndarray): The training labels. It should have a shape of (n_samples, 3). The three columns are the label, patient id, and trial id.
+            shuffle_func (str): specify the shuffle function.
+            masks (list): A list of masking functions applied (str).
+            epochs (Union[int, NoneType]): The number of epochs. When this reaches, the training stops.
+            verbose (bool): Whether to print the training loss after each epoch.
+            
+        Returns:
+            epoch_loss_list: a list containing the training losses on each epoch.
+        '''
+        assert X.ndim == 3
+        assert y.shape[1] == 3
+        
+        # Shuffle the training set for contrastive learning pretraining.
+        X, y = shuffle_feature_label(X, y, shuffle_function=shuffle_function, batch_size=self.batch_size)
+
+        # we need patient id for patient-level contrasting and trial id for trial-level contrasting
+        train_dataset = TensorDataset(
+            torch.from_numpy(X).to(torch.float),
+            torch.from_numpy(y).to(torch.long)
+            )
+        
+        if shuffle_function == 'random':
+            train_loader = DataLoader(train_dataset, batch_size=self.batch_size, shuffle=True, drop_last=True)
+        else:
+            # Important!!! A customized batch_sampler to shuffle samples before each epoch. Check details in utils.py.
+            my_sampler = MyBatchSampler(range(len(train_dataset)), batch_size=self.batch_size, drop_last=True)
+            train_loader = DataLoader(train_dataset, batch_sampler=my_sampler)
+        
+        optimizer = torch.optim.AdamW(self._net.parameters(), lr=self.lr)
+        
+        epoch_loss_list = []
+        start_time = datetime.now()           
+        for epoch in range(epochs):
+            cum_loss = 0
+            for x, y in tqdm(train_loader, desc=f'=> Epoch {epoch+1}', leave=False):
+                # count by iterations
+                x = x.to(self.device)
+                pid = y[:, 1]  # patient id
+
+                optimizer.zero_grad()
+                
+                views = self._net(x, masks=masks)
+                loss = id_contrastive_loss(views, pid)
+                loss.backward()
+                optimizer.step()
+                self.net.update_parameters(self._net)
+
+                cum_loss += loss.item()
+           
+            cum_loss /= len(train_loader)
+            epoch_loss_list.append(cum_loss)
+            
+            if verbose:
+                print(f"=> Epoch {epoch+1}: loss: {cum_loss}")
+                
+            if self.callback_func is not None:
+                self.callback_func(self, epoch)
+                
+        end_time = datetime.now()
+        print(f'=> Training finished in {end_time - start_time}')
+            
+        return epoch_loss_list
